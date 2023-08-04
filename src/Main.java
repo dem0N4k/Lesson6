5a26751ec1c8ad6c20e802a47b3630d0445b10f3
dem0N4k <demON4k1993@gmail.com>
2023-08-04 23:43:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int twixPrice = 77;
+        int marsPrice = 78;
+        int snickersPrice = 100;
+        int bonAquaPrice = 50;
+
+        boolean isCanBuy = false;
+
+        System.out.println("Введите кол-во денег");
+        int moneyAmount = new Scanner(System.in).nextInt();
+
+        if (moneyAmount >= twixPrice) {
+            System.out.println("Вы можете купить твикс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= marsPrice) {
+            System.out.println("Вы можете купить марс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= snickersPrice) {
+            System.out.println("Вы можете купить сникерс!");
+            isCanBuy = true;
+        }
+        if (moneyAmount >= bonAquaPrice) {
+            System.out.println("Вы можете купить воду!");
+            isCanBuy = true;
+        }
+        if (isCanBuy == false) {
+            System.out.println("Вам недостаточно денег!");
+        }
+
+    }
+}
